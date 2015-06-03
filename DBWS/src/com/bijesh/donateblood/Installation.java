dd875c5b1eb8dd075126894cf0aa04cc282da106
bijeshcj <bijesh.jayaprakash@gmail.com>
2015-06-03 16:33:53 +0530

    installation will add new jhard-coded record

--- DBWS/src/com/bijesh/donateblood/Installation.java
+++ DBWS/src/com/bijesh/donateblood/Installation.java
@@ -1,6 +1,7 @@
 package com.bijesh.donateblood;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
 import com.bijesh.donateblood.dao.InstallationDAO;
+
 
 /**
  * Servlet implementation class Installation
@@ -37,10 +41,43 @@ public class Installation extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		TODO: get the response and initialise the Installation class 
+		
 		com.bijesh.donateblood.models.Installation installation = new com.bijesh.donateblood.models.Installation();
-		installation.setUniqueId(request.getParameter("uniqueId"));
-		installation.setTime(request.getParameter("time"));
-		installation.setTimezone(request.getParameter("timezone"));
+		
+//		installation.setUniqueId(request.getParameter("uniqueID"));
+//		installation.setTime(request.getParameter("time"));
+//		installation.setTimezone(request.getParameter("timezone"));
+		System.out.println("uniID from request "+request.getParameter("uniqueID"));
+		System.out.println("data "+request.getParameterNames());
+		
+		Enumeration es = request.getParameterNames();
+		String responseAsString = new String();
+		
+		while (es.hasMoreElements()) {
+			Object object = (Object) es.nextElement();
+			responseAsString = object.toString();
+		}
+		
+		try{
+			
+	    System.out.println("$$$ response as string "+responseAsString);
+			
+		JSONObject jsonObject = new JSONObject(responseAsString);
+		JSONObject data = jsonObject.getJSONObject("Data");
+		
+		String uniqueId = data.getString("uniqueID");
+		String time = data.getString("time");
+		String timeZone = data.getString("timezone");
+		
+		installation.setUniqueId(uniqueId);
+		installation.setTime(time);
+		installation.setTimezone(timeZone);
+		
+		System.out.println("$$$ uniqueId "+uniqueId);
+		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		
 		
 		new InstallationDAO().insertDeviceInstallation(installation);
